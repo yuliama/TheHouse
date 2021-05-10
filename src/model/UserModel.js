c5545cc0821ab104edfb9d72abc9b7e8892ec7d9
yulia <yulka84@gmail.com>
2021-05-10 13:30:54 +0300

    add usermodel functions: login, logout, resetpasw

--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -1,9 +1,33 @@
-export default class UserModel{
-    #pwd;
-    constructor(plainUser){
-        this.id = plainUser.id;
-        this.fullName = plainUser.fullName;
-        this.apartment = plainUser.apartment;
-        this.isCommiteeMember = plainUser.isCommiteeMember;
+import Parse from 'parse';
+
+export default class UserModel {
+    #parseUser;
+    constructor(parseUser) {
+        this.id = parseUser.id;
+        this.fullName = parseUser.fullName;
+        this.apartment = parseUser.apartment;
+        this.isCommiteeMember = parseUser.isCommiteeMember;
+        this.#parseUser = parseUser;
+    }
+
+    static activeUser = null;
+
+    static async login(email, pwd) {
+        const parseUser = await Parse.User.logIn(email, pwd);
+        UserModel.activeUser = new UserModel(parseUser);
+        return UserModel.activeUser;
+    }
+    static logout() {
+        UserModel.activeUser = null;
+        Parse.User.logout();
+    }
+
+    static loadActiveUser() {
+        UserModel.activeUser = Parse.User.current() ? new UserModel(Parse.User.current()) : null;
+        return UserModel.activeUser;
+    }
+
+    static async resetPassword(email){
+        return await Parse.User.requestPasswordReset(emailValue);
     }
 }
