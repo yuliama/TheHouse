fddfaa67388c409beb304ecaf77a9c0575e22afa
yulia <yulka84@gmail.com>
2021-05-13 21:54:12 +0300

    add comunityModel manageUsersPage -> print all communityUsers

---
+++ src/model/CommunityModel.js
@@ -0,0 +1,18 @@
+import Parse from 'parse';
+
+export default class CommunityModel {
+    constructor(parseCommunity) {
+        this.id = parseCommunity.id;
+        this.Address = parseCommunity.get("Address");
+    }
+
+    static async getCommunityDetails(communityId){
+        const communityTable = Parse.Object.extend('Community');
+        const query = new Parse.Query(communityTable);
+        query.contains("objectId", communityId);
+        const parseCom = await query.find();
+        const community = parseCom.map(item => new CommunityModel(item));
+        console.log(community);
+        return community;
+    }
+}
--- src/pages/ManageUsers/ManageUsers.js
+++ src/pages/ManageUsers/ManageUsers.js
@@ -1,14 +1,17 @@
 import { useEffect, useState } from 'react';
 import { Redirect } from 'react-router';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import CommunityModel from '../../model/CommunityModel';
 
 export default function ManageUsers({ activeUser }) {
 
     const [communityUsers, setCommunityUsers] = useState([]);
+    const [communityDetails, setCommunityDetails] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
             setCommunityUsers(await activeUser.GetComminityUsers(activeUser.Community.id));
+            setCommunityDetails(await CommunityModel.getCommunityDetails(activeUser.Community.id));
         }
 
         if (activeUser) {
@@ -25,13 +28,32 @@ export default function ManageUsers({ activeUser }) {
 
     return (
         <Container className="p-manageUsers">
-            <Row>
-                {communityUsers.map(user =>
-                    <Col key={user.id} md={3} sm={6}>
-                        {user.fullName}{', '}{user.apartment}
-                    </Col>
-                )}
+            <h1>רשימת דיירי בניין בכתובת: {communityDetails.map(item => item.Address)}</h1>
+            <Row className="bg-light text-dark border">
+                <Col md={1}>מס"ד</Col>
+                <Col md={2}>שם</Col>
+                <Col md={2}>דוא"ל</Col>
+                <Col md={1}>מספר דירה</Col>
+                <Col md={6}></Col>
             </Row>
+            {communityUsers.map((user, index) =>
+                <Row key={user.id} className="bg-light text-dark border">
+                    <Col md={1}>{index + 1}</Col>
+                    <Col md={2}>{user.fullName}</Col>
+                    <Col md={2}>{user.email}</Col>
+                    <Col md={1}>{user.apartment}</Col>
+                    <Col md={6}>
+                        <Button>עריכה</Button>
+                        <Button>מחיקה</Button>
+                    </Col>
+                </Row>
+            )}
+            <Row className="bg-light text-dark border">
+                    <Col md={1}></Col>
+                    <Col md={2}></Col>
+                    <Col md={2}><a href="">+הוסף דייר</a></Col>
+                </Row>
+
         </Container>
     )
 
