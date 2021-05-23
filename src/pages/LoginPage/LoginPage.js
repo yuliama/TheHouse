3439c912a1e49347a145e62b0fb69d5ff71fd987
yulia <yulka84@gmail.com>
2021-05-23 09:04:40 +0300

    fix login page css

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Redirect } from 'react-router';
 import { Form, Button, Alert } from 'react-bootstrap';
 import UserModel from '../../model/UserModel';
+import './LoginPage.css';
 
 export default function LoginPage({activeUser, onLogin}) {
     const [email, setEmail] = useState();
@@ -29,26 +30,23 @@ export default function LoginPage({activeUser, onLogin}) {
     return (
         <div className="p-login">
             
-            <h1>Please Login</h1>
+            <h1>אנא התחבר</h1>
             {/* <p>or <Link to="/signup">create an account</Link></p> */}
             {showInvalidLogin ? <Alert variant="danger">Invalid Credentials!</Alert> : null}
             <Form onSubmit={login}>
                 <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email"
+                    <Form.Label>דוא"ל</Form.Label>
+                    <Form.Control type="email" placeholder="הזן דואר אלקטרוני"
                         value={email} onChange={e => setEmail(e.target.value)} />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password"
+                    <Form.Label>סיסמה</Form.Label>
+                    <Form.Control type="password" placeholder="הזן סיסמה"
                         value={pwd} onChange={e => setPwd(e.target.value)} />
                 </Form.Group>
                 <Button variant="success" type="submit" block>
-                    Login
+                    התחבר
                 </Button>
             </Form>
         </div>
